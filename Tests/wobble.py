fe2601345edc99a9f5977da94ecde0ea063aaf93
TheBrainyOne <albert.chejin.edward.smith@outlook.com>
2016-04-12 18:27:11 +0800

    Fix up my files
    I'm using Axosoft Gitkraken and accidentally unleashed hell on the repo, hence the mess of merging below.

--- Tests/wobble.py
+++ Tests/wobble.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath
 
 from PyPhys import *
 from PyPhys.util import Vector2D
+import pygame
 
 def dragBox(box, cam):
 	pos = cam.posFromPygame(cam.getMousePos())
